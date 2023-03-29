13a248cec79ee7a7b45e05af97156200ffc16df3
Lexakr <alexeynealeksey@gmail.com>
2023-03-30 04:17:03 +0700

    Чистка кода

--- LoginWindowVM.cs
+++ LoginWindowVM.cs
@@ -1,12 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace TMP_Lab2
 {
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WpfApp2;
 
 namespace TMP_Lab2
